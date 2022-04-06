7811014bf34ec1dac903fc6f69bf7bda7b3023fa
J-aros <j.aros.cortes@gmail.com>
2022-04-06 13:50:39 -0400

    chore: local db is added

--- src/database.js
+++ src/database.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 //Mongodb connection
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    const connect = await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase"
+    );
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (err) {
     console.log(err);
