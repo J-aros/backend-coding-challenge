be7f28df4da83e5141b8c866c0daf8a7ae73f0be
J-aros <j.aros.cortes@gmail.com>
2022-04-05 22:59:27 -0400

    feat: database connection

---
+++ src/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Mongodb connection
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
