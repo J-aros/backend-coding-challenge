9496568d01e4a2c187909324d38061f5393de445
J-aros <j.aros.cortes@gmail.com>
2022-04-06 14:52:42 -0400

    fix: fix created at of a payment and required of echange_currency

--- src/models/payment.js
+++ src/models/payment.js
@@ -1,47 +1,55 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = mongoose.Schema({
-  object: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  billed_hours: {
-    type: Number,
-    required: true,
-  },
-  billed_at: {
-    type: String,
-    required: true,
-  },
-  billing_currency: {
-    type: String,
-    required: true,
-  },
-  billed_amount: {
-    type: Number,
-    required: true,
-  },
-  needs_exchange: {
-    type: Boolean,
-    required: true,
-  },
-  echange_currency: {
-    type: String,
-    required: paymentSchema.needs_exchange.required ? true : false,
-  },
-  exchange: {
-    original_amount: {
-      type: Number,
-    }, 
-    currency: {
+const paymentSchema = mongoose.Schema(
+  {
+    object: {
       type: String,
-    }, 
-    exchange_rate: {
+      default: "payment",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    billed_hours: {
       type: Number,
-    }, 
+      required: true,
+    },
+    billed_at: {
+      type: String,
+      required: true,
+    },
+    billing_currency: {
+      type: String,
+      required: true,
+    },
+    billed_amount: {
+      type: Number,
+      required: true,
+    },
+    needs_exchange: {
+      type: Boolean,
+      required: true,
+    },
+    echange_currency: {
+      type: String,
+      required: false,
+    },
+    exchange: {
+      type: Object,
+      required: false,
+      original_amount: {
+        type: Number,
+      },
+      currency: {
+        type: String,
+      },
+      exchange_rate: {
+        type: Number,
+      },
+    },
   },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Payment", paymentSchema);
