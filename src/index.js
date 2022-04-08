d6d2d23cffc05777b2268b2a325d818c5269958d
J-aros <j.aros.cortes@gmail.com>
2022-04-08 07:38:53 -0400

    feat: add limit of 10 request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const paymentRoutes = require("./routes/PaymentRoutes/payment.routes");
 const connectDB = require("./lib/Database/database");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+//Limit 10 requests
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+  handler: function (req, res) {
+    return res.status(429).json({
+      error: "To many requests, please try again in a minute",
+    });
+  },
+});
+
+app.use(limiter);
 
 //mongodb connection
 connectDB();
